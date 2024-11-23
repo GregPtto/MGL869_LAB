b489a5c1db87f0ceed5bbe976aa7a65c5f68c8ed
guillaume <guillaume.bellone02@gmail.com>
2024-11-23 16:33:55 -0500

    Script d'installation des dépendances python du projet

---
+++ Scripts/dependencyInstallation.py
@@ -0,0 +1,7 @@
+import subprocess
+
+if __name__ == '__main__':
+    subprocess.run(["pip.exe", "install", "panda"])
+    subprocess.run(["pip.exe", "install", "scikit-learn"])
+    subprocess.run(["pip.exe", "install", "matplotlib"])
+    subprocess.run(["pip.exe", "install", "multiprocessing"])
